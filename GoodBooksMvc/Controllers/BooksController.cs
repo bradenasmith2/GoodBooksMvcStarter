94135c12209c40c13c3b21de71a5998358761ff4
bradenasmith2 <bradenasmith@icloud.com>
2023-09-05 16:51:52 -0500

    completed lab

---
+++ GoodBooksMvc/Controllers/BooksController.cs
@@ -0,0 +1,61 @@
+﻿using GoodBooksMvc.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using GoodBooksMvc.Models;
+
+namespace GoodBooksMvc.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly GoodBooksMvcContext _context;
+
+        public BooksController(GoodBooksMvcContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var favoritedBooks = Request.Cookies["FavoritedBooks"];
+            ViewData["favoritedBooks"] = favoritedBooks;
+
+            return View(_context.Books.ToList());
+        }
+
+        [Route("/books/compare/{id:int}")]
+        public IActionResult Favorite(int id)
+        {
+            var book = _context.Books.Find(id);
+            var recievedCookie = Request.Cookies["FavoritedBooks"].Split(",").ToList();
+            recievedCookie.Add(book.Title);
+
+            string cookieString = "";
+            foreach(var e in recievedCookie)
+            {
+                cookieString += e;
+                cookieString += ",";
+            }
+
+            Response.Cookies.Append("FavoritedBooks", cookieString);
+
+            return Redirect("/books");
+        }
+
+        [Route("/books/compare/remove/{id:int}")]
+        public IActionResult RemoveFavorite(int id)
+        {
+            var book = _context.Books.Find(id);
+            var recievedCookie = Request.Cookies["FavoritedBooks"].Split(",").ToList();
+            recievedCookie.Remove(book.Title);
+
+            string cookieString = "";
+            foreach (var e in recievedCookie)
+            {
+                cookieString += e;
+                cookieString += ",";
+            }
+
+            Response.Cookies.Append("FavoritedBooks", cookieString);
+
+            return Redirect("/books");
+        }
+    }
+}
--- GoodBooksMvc/Program.cs
+++ GoodBooksMvc/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSession();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -18,6 +19,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSession();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
